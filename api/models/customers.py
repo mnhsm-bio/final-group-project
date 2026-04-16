2ebe78f90f80291a4c4c6f8822118d3a5beb6b3c
Gnana Chand Mallangi <chintu13.mallangi@gmail.com>
2026-04-15 21:42:53 -0400

    Added customer endpoints

---
+++ api/models/customers.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from ..dependencies.database import Base
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    phone = Column(String(20), nullable=True)
+    address = Column(String(255), nullable=True)
